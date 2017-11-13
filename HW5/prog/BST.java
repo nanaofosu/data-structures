aa581fc8b66222210daf2554e380a1422bbbbea6
Nana Ofosu Budu <nanaofosu@uchicago.edu>
2017-11-12 22:40:20 -0600

    Problem 2 works

--- HW5/prog/BST.java
+++ HW5/prog/BST.java
@@ -3,19 +3,19 @@
 
 import java.util.*;
 
-public class BST 
+public class BST
 {
     // instance variables
     private BSTNode m_root;
     private int m_size;
-    
+
     // constructor
     public BST()
     {
         m_root = null;
         m_size = 0;
     }
-    
+
     // add a value into the tree
     public void add(int v)
     {
@@ -71,7 +71,16 @@ public class BST
 
 
     }
-    
+    /*
+    . While current is not NULL
+   If current does not have left child
+      a) Print current’s data
+      b) Go to the right, i.e., current = current->right
+   Else
+      a) Make current as right child of the rightmost
+         node in current's left subtree
+      b) Go to this left child, i.e., current = current->left
+     */
     // print the tree content using in-order traversal
     public void inOrder()
     {
@@ -87,24 +96,46 @@ public class BST
         while(current != null){
             // check if the left is null
             if(current.getLeft() == null){
-                output += current.getInfo(); // append to output.
+                output += current.getInfo() + " "; // append to output.
                 //set current to the right
                 current = current.getRight();
             }
             else{
-                
+                 /* Find the inorder predecessor of current */
+                parent = current.getLeft();
+
+                while (parent.getRight() != null && parent.getRight() != current)
+                    parent = parent.getRight();
+
+                /* Make current as right child of its inorder predecessor */
+                if (parent.getRight() == null)
+                {
+                    parent.setRight(current);
+                    current = current.getLeft();
+                }
+
+                 /* Revert the changes made in if part to restore the
+                    original tree i.e.,fix the right child of predecssor*/
+                 else
+                 {
+                    parent.setRight(null);
+                    // System.out.print(current.data + " ");
+                    output +=current.getInfo() + " ";
+                    current = current.getRight();
+                }   /* End of if condition pre->right == NULL */
+
             }
         }
 
-
+        System.out.print(output);
     }
-    
+
     // get the size of the tree
     public int size()
     {
         return m_size;
     }
-    
+
     // empty the tree
     public void clear()
     {
